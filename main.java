39309a98804d56d35a7fc9441409c202e99d3b0a
evangeissler <geisslee@uregina.ca>
2017-10-19 22:33:05 -0600

    Added more testing lines. Fixed issue in main that caused an infinite loop

--- main.java
+++ main.java
@@ -1,15 +1,33 @@
 public class Main{
 
 public static void main(String[] args){
+	//Create elements to put into linked list
 	ListElement le = new ListElement();
+	ListElement le2 = new ListElement();
+	ListElement le3 = new ListElement();
+	ListElement le4 = new ListElement(); 
 
-
+	//Assign values to elements
 	le.setData(5);
-//	System.out.print("Data at node: " + le.getData());
-	
+	le2.setData(2);
+	le3.setData(3);
+	le4.setData(4);
+
+	//Print elements from ListElement Class
+	System.out.print("Values from getData() (no LL): ");
+	System.out.print(le.getData() + " ");
+	System.out.print(le2.getData() + " ");
+	System.out.print(le3.getData() + " ");
+	System.out.print(le4.getData() + " ");
+
+	//Create Linked list
 	LinkedList list = new LinkedList();
 
+	//Add elements to List
 	list.addElement(le);
+	System.out.print("Adding an elements...\n");
+	//list.addElement(le);
 	list.printLinkedListTail();
-	}
+
+}
 }
